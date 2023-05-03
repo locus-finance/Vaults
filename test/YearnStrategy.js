8fcd45f6551db5485a5910692548b936d4265f27
Temirzhan Yussupov <timka2609@gmail.com>
2023-05-03 13:57:15 +0600

    feat: add first test for YearnStrategy

---
+++ test/YearnStrategy.js
@@ -0,0 +1,71 @@
+const {
+    loadFixture,
+    mine,
+} = require("@nomicfoundation/hardhat-network-helpers");
+const { ZERO_ADDRESS } = require("@openzeppelin/test-helpers/src/constants");
+const { expect } = require("chai");
+const { BigNumber } = require("ethers");
+const { ethers } = require("hardhat");
+
+const IERC20_SOURCE = "@openzeppelin/contracts/token/ERC20/IERC20.sol:IERC20";
+
+describe("YearnStrategy", function () {
+    async function deployContractAndSetVariables() {
+        const [deployer, governance, treasury, whale] =
+            await ethers.getSigners();
+        const WETH_ADDRESS = "0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2";
+        const want = await ethers.getContractAt("IWETH", WETH_ADDRESS);
+        await want
+            .connect(whale)
+            .deposit({ value: ethers.utils.parseEther("10") });
+
+        const name = "USDC Vault";
+        const symbol = "vUSDC";
+        const BaseVault = await ethers.getContractFactory("BaseVault");
+        const vault = await BaseVault.deploy();
+        await vault.deployed();
+
+        await vault["initialize(address,address,address,string,string)"](
+            want.address,
+            deployer.address,
+            treasury.address,
+            name,
+            symbol
+        );
+        await vault["setDepositLimit(uint256)"](
+            ethers.utils.parseEther("10000")
+        );
+
+        const YearnStrategy = await ethers.getContractFactory("YearnStrategy");
+        const strategy = await YearnStrategy.deploy(vault.address);
+        await strategy.deployed();
+
+        await vault["addStrategy(address,uint256,uint256,uint256,uint256)"](
+            strategy.address,
+            10000,
+            0,
+            ethers.utils.parseEther("10000"),
+            0
+        );
+
+        return {
+            vault,
+            deployer,
+            symbol,
+            name,
+            want,
+            whale,
+            governance,
+            treasury,
+            strategy,
+            want,
+        };
+    }
+
+    it("should deploy strategy", async function () {
+        const { vault, strategy } = await loadFixture(
+            deployContractAndSetVariables
+        );
+        expect(await strategy.vault()).to.equal(vault.address);
+    });
+});
