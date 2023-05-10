0d0efc265080cbebcdc82ef5b0cc26b3b03533d9
Temirzhan Yussupov <timka2609@gmail.com>
2023-05-10 13:17:56 +0600

    feat: start working

---
+++ contracts/strategies/CVXStrategy.sol
@@ -0,0 +1,147 @@
+// SPDX-License-Identifier: AGPL-3.0
+
+pragma solidity ^0.8.12;
+
+import {BaseStrategy, StrategyParams, VaultAPI} from "@yearn-protocol/contracts/BaseStrategy.sol";
+import {ERC20, IERC20} from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import {Math} from "@openzeppelin/contracts/utils/math/Math.sol";
+import {SafeERC20} from "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
+import {OracleLibrary} from "@uniswap/v3-periphery/contracts/libraries/OracleLibrary.sol";
+
+import "hardhat/console.sol";
+
+import "../integrations/balancer/IBalancerPriceOracle.sol";
+import "../integrations/curve/ICurve.sol";
+
+contract CVXStrategy is BaseStrategy {
+    using SafeERC20 for IERC20;
+
+    uint32 internal constant TWAP_RANGE_SECS = 1800;
+    uint256 public slippage = 9500; // 5%
+
+    constructor(address _vault) BaseStrategy(_vault) {}
+
+    function setSlippage(uint256 _slippage) external onlyStrategist {
+        require(_slippage < 10_000, "!_slippage");
+        slippage = _slippage;
+    }
+
+    function name() external pure override returns (string memory) {
+        return "StrategyCVX";
+    }
+
+    function balanceOfWant() public view returns (uint256) {
+        return want.balanceOf(address(this));
+    }
+
+    function _withdrawSome(uint256 _amountNeeded) internal {}
+
+    function _exitPosition(uint256) internal {}
+
+    function ethToWant(
+        uint256 _amtInWei
+    ) public view override returns (uint256) {
+        IBalancerPriceOracle.OracleAverageQuery[] memory queries;
+        queries = new IBalancerPriceOracle.OracleAverageQuery[](1);
+        queries[0] = IBalancerPriceOracle.OracleAverageQuery({
+            variable: IBalancerPriceOracle.Variable.PAIR_PRICE,
+            secs: TWAP_RANGE_SECS,
+            ago: 0
+        });
+
+        uint256[] memory results;
+        results = IBalancerPriceOracle(USDC_WETH_BALANCER_POOL)
+            .getTimeWeightedAverage(queries);
+
+        return
+            Utils.scaleDecimals(
+                (_amtInWei * results[0]) / 1e18,
+                ERC20(WETH),
+                ERC20(address(want))
+            );
+    }
+
+    function estimatedTotalAssets()
+        public
+        view
+        override
+        returns (uint256 _wants)
+    {
+        _wants = balanceOfWant();
+    }
+
+    function prepareReturn(
+        uint256 _debtOutstanding
+    )
+        internal
+        override
+        returns (uint256 _profit, uint256 _loss, uint256 _debtPayment)
+    {
+        uint256 _totalAssets = estimatedTotalAssets();
+        uint256 _totalDebt = vault.strategies(address(this)).totalDebt;
+
+        if (_totalAssets >= _totalDebt) {
+            _profit = _totalAssets - _totalDebt;
+            _loss = 0;
+        } else {
+            _profit = 0;
+            _loss = _totalDebt - _totalAssets;
+        }
+
+        _withdrawSome(_debtOutstanding + _profit);
+
+        uint256 _liquidWant = want.balanceOf(address(this));
+
+        // enough to pay profit (partial or full) only
+        if (_liquidWant <= _profit) {
+            _profit = _liquidWant;
+            _debtPayment = 0;
+            // enough to pay for all profit and _debtOutstanding (partial or full)
+        } else {
+            _debtPayment = Math.min(_liquidWant - _profit, _debtOutstanding);
+        }
+    }
+
+    function adjustPosition(uint256 _debtOutstanding) internal override {
+        uint256 _wantBal = balanceOfWant();
+
+        if (_wantBal > _debtOutstanding) {
+            uint256 _excessWant = _wantBal - _debtOutstanding;
+        }
+    }
+
+    function liquidateAllPositions() internal override returns (uint256) {
+        return want.balanceOf(address(this));
+    }
+
+    function liquidatePosition(
+        uint256 _amountNeeded
+    ) internal override returns (uint256 _liquidatedAmount, uint256 _loss) {
+        uint256 _wantBal = want.balanceOf(address(this));
+        if (_wantBal >= _amountNeeded) {
+            return (_amountNeeded, 0);
+        }
+
+        _withdrawSome(_amountNeeded - _wantBal);
+        _wantBal = want.balanceOf(address(this));
+
+        if (_amountNeeded > _wantBal) {
+            _liquidatedAmount = _wantBal;
+            _loss = _amountNeeded - _wantBal;
+        } else {
+            _liquidatedAmount = _amountNeeded;
+        }
+    }
+
+    function prepareMigration(address _newStrategy) internal override {}
+
+    function protectedTokens()
+        internal
+        pure
+        override
+        returns (address[] memory)
+    {
+        address[] memory protected = new address[](2);
+        return protected;
+    }
+}
