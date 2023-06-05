ac54498dad058d1bddcc22270b79927cd1e2cbf1
Ivan Borisov <2622610@gmail.com>
2023-06-05 17:58:29 +0300

    deploye davault

---
+++ scripts/deploy/daVault/Vault.js
@@ -0,0 +1,72 @@
+const hre = require("hardhat");
+
+const { getEnv } = require("../../utils");
+
+const USDC_DECIMALS = 6;
+const USDC_ADDRESS = "0xFF970A61A04b1cA14834A43f5dE4533eBDDB5CC8";
+
+const DEPLOY_SETTINGS = {
+    want: USDC_ADDRESS,
+    name: "daVault",
+    symbol: "daToken",
+    governance: getEnv("GOVERNANCE_ACCOUNT"),
+    treasury: getEnv("TREASURY_ACCOUNT"),
+    depositLimitUsdc: "1000000000",
+    performanceFee: 1500,
+    managementFee: 200,
+};
+
+async function main() {
+    const [deployer] = await hre.ethers.getSigners();
+    const {
+        want,
+        name,
+        symbol,
+        governance,
+        treasury,
+        depositLimitUsdc,
+        managementFee,
+        performanceFee,
+    } = DEPLOY_SETTINGS;
+
+    const Vault = await hre.ethers.getContractFactory("Vault");
+    const vault = await Vault.deploy();
+    await vault.deployed();
+
+    const tx = await vault["initialize(address,address,address,string,string)"](
+        want,
+        governance,
+        treasury,
+        name,
+        symbol
+    );
+    await tx.wait();
+
+    console.log(`${name} deployed to ${vault.address} by ${deployer.address}`);
+    console.log(`Vault symbol: ${await vault.symbol()}`);
+
+    const limitTx = await vault["setDepositLimit(uint256)"](
+        ethers.utils.parseUnits(depositLimitUsdc, USDC_DECIMALS)
+    );
+    await limitTx.wait();
+    console.log(`Set deposit limit to ${await vault.depositLimit()}`);
+
+    const performanceFeeTx = await vault["setPerformanceFee(uint256)"](
+        performanceFee
+    );
+    await performanceFeeTx.wait();
+    console.log(`Set performance fee to ${await vault.performanceFee()}`);
+
+    const managementFeeTx = await vault["setManagementFee(uint256)"](
+        managementFee
+    );
+    await managementFeeTx.wait();
+    console.log(`Set management fee to ${await vault.managementFee()}`);
+}
+
+main()
+    .then(() => process.exit(0))
+    .catch((error) => {
+        console.error(error);
+        process.exitCode = 1;
+    });
