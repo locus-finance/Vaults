87dd218e8e80d476c41ca14bfa574996ee360184
Temirzhan Yussupov <timka2609@gmail.com>
2023-06-29 23:54:00 +0600

    fix: implement correct slippage calculation for CVX to CRV

--- contracts/utils/CVXRewards.sol
+++ contracts/utils/CVXRewards.sol
@@ -3,13 +3,18 @@
 pragma solidity ^0.8.18;
 
 import {SafeMath} from "@openzeppelin/contracts/utils/math/SafeMath.sol";
+import {OracleLibrary} from "@uniswap/v3-periphery/contracts/libraries/OracleLibrary.sol";
 
 import "../integrations/aura/ICvx.sol";
 
 /// @notice Used for calculating rewards.
 /// @dev This implementation is taken from CVX's contract (https://etherscan.io/address/0x4e3FBD56CD56c3e72c1403e103b45Db9da5B9D2B#code).
 library CVXRewardsMath {
+    uint32 internal constant TWAP_RANGE_SECS = 1800;
     address internal constant CVX = 0x4e3FBD56CD56c3e72c1403e103b45Db9da5B9D2B;
+    address internal constant CRV = 0xD533a949740bb3306d119CC777fa900bA034cd52;
+    address internal constant CVX_CRV_UNI_V3_POOL =
+        0x645c3A387b8633dF1D4D71CA4b50D27233Bcb887;
 
     using SafeMath for uint256;
 
@@ -30,5 +35,19 @@ library CVXRewardsMath {
             }
         }
         return _amount;
+    }
+
+    function cvxToCrv(uint256 cvxTokens) internal view returns (uint256) {
+        (int24 meanTick, ) = OracleLibrary.consult(
+            CVX_CRV_UNI_V3_POOL,
+            TWAP_RANGE_SECS
+        );
+        return
+            OracleLibrary.getQuoteAtTick(
+                meanTick,
+                uint128(cvxTokens),
+                CVX,
+                CRV
+            );
     }
 }
