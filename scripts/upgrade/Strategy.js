4d3cf520329e78e5241c9ed1575b4fa4a94c06b2
Ivan Borisov <2622610@gmail.com>
2023-08-15 18:45:24 +0300

    upgraded script

--- scripts/upgrade/Strategy.js
+++ scripts/upgrade/Strategy.js
@@ -2,17 +2,26 @@ const hre = require("hardhat");
 
 const { getEnv } = require("../utils");
 
-const TARGET_STRATEGY = getEnv("TARGET_STRATEGY");
+//const TARGET_STRATEGY = getEnv("TARGET_STRATEGY");
 const TARGET_ADDRESS = getEnv("TARGET_ADDRESS");
 
 async function main() {
-    const TargetContract = await ethers.getContractFactory(TARGET_STRATEGY);
-    const strategy = TargetContract.attach(TARGET_ADDRESS);
-    const vault = await strategy.vault();
 
-    await hre.upgrades.forceImport(TARGET_ADDRESS, TargetContract, {
-        kind: "transparent",
-    });
+  console.log('Running deploy script');
+  const TargetContract = await hre.ethers.getContractFactory("FXSStrategy");
+  const strategy = TargetContract.attach(TARGET_ADDRESS);
+  const vault = await strategy.vault();
+  console.log("Preparing upgrade...");
+
+  console.log('Implementation address: ' + await hre.upgrades.erc1967.getImplementationAddress(TARGET_ADDRESS));
+  console.log('Admin address: ' + await hre.upgrades.erc1967.getAdminAddress(TARGET_ADDRESS));
+
+  const [deployer] = await hre.ethers.getSigners();
+  await hre.upgrades.forceImport(TARGET_ADDRESS, TargetContract, {
+    kind: "transparent",
+    constructorArgs: [vault],
+    from: deployer,
+  });
 
     const upgraded = await hre.upgrades.upgradeProxy(
         TARGET_ADDRESS,
@@ -23,7 +32,8 @@ async function main() {
         }
     );
 
-    console.log(`Successfully upgraded implementation of ${TARGET_STRATEGY}`);
+
+    console.log("Successfully upgraded implementation of", upgraded.address);
 
     await hre.run("verify:verify", {
         address: upgraded.address,
