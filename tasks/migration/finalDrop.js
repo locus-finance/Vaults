60005501097f2e3fe4e3573aad0ce4ccdf013ed3
GoldenSylph <simplavero@gmail.com>
2023-11-03 22:01:48 +0400

    adding temporary drop settings

--- tasks/migration/finalDrop.js
+++ tasks/migration/finalDrop.js
@@ -4,7 +4,7 @@ module.exports = (task) =>
     "dropToVaults",
     "Drop tokens on Dropper to Vaults V2.",
   )
-    .addOptionalParam('v2vault', "Define to where the migration should occur.", '0x65b08FFA1C0E1679228936c0c85180871789E1d7', types.string)
+    .addOptionalParam('v2vault', "Define to where the migration should occur.", '0x0e86f93145d097090acbbb8ee44c716dacff04d7', types.string)
     .addOptionalParam('dropper', "Define from where the task should gather the drop receivers.", '0xEB20d24d42110B586B3bc433E331Fe7CC32D1471', types.string)
     .addOptionalParam('csv', "Define a full path with name for CSV file.", './tasks/migration/csv/postMigration/finalMigration/lvEthV2HoldersFinallyMigrated.csv', types.string)
     .setAction(additionalDropActionBuilder());
