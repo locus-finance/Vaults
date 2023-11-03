8f603689aa8aaed6feb05e75361c30acbb050439
GoldenSylph <simplavero@gmail.com>
2023-11-03 23:05:05 +0400

    change of params

--- tasks/migration/finalDrop.js
+++ tasks/migration/finalDrop.js
@@ -4,7 +4,7 @@ module.exports = (task) =>
     "finalDrop",
     "Drop tokens on Dropper to Vaults V2.",
   )
-    .addOptionalParam('v2vault', "Define to where the migration should occur.", '0x65b08FFA1C0E1679228936c0c85180871789E1d7', types.string)
+    .addOptionalParam('v2vault', "Define to where the migration should occur.", '0x0f094f6deb056af1fa1299168188fd8c78542a07', types.string)
     .addOptionalParam('dropper', "Define from where the task should gather the drop receivers.", '0xEB20d24d42110B586B3bc433E331Fe7CC32D1471', types.string)
-    .addOptionalParam('csv', "Define a full path with name for CSV file.", './tasks/migration/csv/postMigration/finalMigration/lvDciV2HoldersFinallyMigrated.csv', types.string)
+    .addOptionalParam('csv', "Define a full path with name for CSV file.", './tasks/migration/csv/postMigration/finalMigration/lvAyiV2HoldersFinallyMigrated.csv', types.string)
     .setAction(additionalDropActionBuilder());
--- tasks/migration/migrateVaults.js
+++ tasks/migration/migrateVaults.js
@@ -4,7 +4,7 @@ module.exports = (task) =>
     "migrateVaults",
     "Migrate funds from Vault V1 to Vault V2.",
   )
-    .addOptionalParam('v1vault', "Define from where the migration should occur.", '0xf62A24EbE766d0dA04C9e2aeeCd5E86Fac049B7B', types.string)
-    .addOptionalParam('v2vault', "Define to where the migration should occur.", '0x65b08FFA1C0E1679228936c0c85180871789E1d7', types.string)
-    .addOptionalParam('migration', "Define from where the task should gather the drop receivers.", '0xf42402303BCA9d5575A8aC7b90CB18026c80354D', types.string)
+    .addOptionalParam('v1vault', "Define from where the migration should occur.", '0xBE55f53aD3B48B3ca785299f763d39e8a12B1f98', types.string)
+    .addOptionalParam('v2vault', "Define to where the migration should occur.", '0x0f094f6deb056af1fa1299168188fd8c78542a07', types.string)
+    .addOptionalParam('migration', "Define from where the task should gather the drop receivers.", '0xC7469254416Ad546A5F07e5530a0c3eA468F1FCE', types.string)
     .setAction(migrationContractInteractionActionBuilder());
