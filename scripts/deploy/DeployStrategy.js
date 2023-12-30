ad736a95b5e89ca42bd10eb73279c0e90acec1ae
GoldenSylph <simplavero@gmail.com>
2023-12-31 02:36:35 +0400

    add scripts

---
+++ scripts/deploy/DeployStrategy.js
@@ -0,0 +1,33 @@
+const { ethers } = require("hardhat");
+
+async function main() {
+  console.log('Starting...');
+  const [deployer] = await ethers.getSigners();
+
+  console.log("Deploying contracts with the account:", deployer.address);
+
+  const strategyFactory = await ethers.getContractFactory("SaverStrategy");
+
+  console.log('Constructed factory...');
+
+  const strategy = await hre.upgrades.deployProxy(
+    strategyFactory,
+    [
+      "0x0e86f93145d097090aCBBB8Ee44c716DACFf04d7", 
+      "0x27f52fd2E60B1153CBD00D465F97C05245D22B82", 
+      "0x5C6412CE0E1f5C15C98AEbc5353d936Ed9bC5Bf1"],
+    {
+      kind: "transparent",
+      initializer: "initialize"
+    }
+  );
+
+  console.log("Strategy address:", await strategy.getAddress());
+}
+
+main()
+  .then(() => process.exit(0))
+  .catch((error) => {
+    console.error(error);
+    process.exit(1);
+  });
