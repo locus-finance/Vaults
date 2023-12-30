b43591c8644402a436e5446a34d5d78c47af1490
GoldenSylph <simplavero@gmail.com>
2023-12-31 02:41:33 +0400

    fix

--- scripts/deploy/DeployStrategy.js
+++ scripts/deploy/DeployStrategy.js
@@ -10,15 +10,19 @@ async function main() {
 
   console.log('Constructed factory...');
 
+  const vault = "0x0e86f93145d097090aCBBB8Ee44c716DACFf04d7";
+
   const strategy = await hre.upgrades.deployProxy(
     strategyFactory,
     [
-      "0x0e86f93145d097090aCBBB8Ee44c716DACFf04d7", 
+      vault, 
       "0x27f52fd2E60B1153CBD00D465F97C05245D22B82", 
-      "0x5C6412CE0E1f5C15C98AEbc5353d936Ed9bC5Bf1"],
+      "0x5C6412CE0E1f5C15C98AEbc5353d936Ed9bC5Bf1"
+    ],
     {
       kind: "transparent",
-      initializer: "initialize"
+      initializer: "initialize",
+      constructorArgs: [vault]
     }
   );
 
