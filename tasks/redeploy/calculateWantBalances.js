e5a07e4ff33f4d063f7f173415829129854923b7
Oleg Bedrin <numeralhuman@gmail.com>
2024-01-18 17:09:56 +0400

    adding want tokens csvs

--- tasks/redeploy/calculateWantBalances.js
+++ tasks/redeploy/calculateWantBalances.js
@@ -6,12 +6,12 @@ module.exports = (task) =>
     "calculateWantBalances",
     "Calculate balances of resulting CSVs in want tokens.",
   )
-    .addOptionalParam('result', "Define a path where validated CSV of want tokens balances and respective addresses would be stored.", './tasks/redeploy/csv/output/holdersLvDCI-ETH-result-want-tokens.csv', types.string)
-    .addOptionalParam('csv', "Define a path where validated CSV of balances and addresses are stored.", './tasks/redeploy/csv/output/holdersLvDCI-ETH-result-vault-tokens.csv', types.string)
-    .addOptionalParam('pps', "Define a PPS float value for conversion.", 0.8, types.float)
+    .addOptionalParam('result', "Define a path where validated CSV of want tokens balances and respective addresses would be stored.", './tasks/redeploy/csv/output/holdersLvAYI-ARB-result-want-tokens.csv', types.string)
+    .addOptionalParam('csv', "Define a path where validated CSV of balances and addresses are stored.", './tasks/redeploy/csv/output/holdersLvAYI-ARB-result-vault-tokens.csv', types.string)
+    .addOptionalParam('pps', "Define a PPS float value for conversion.", 0.992011, types.float)
     .addOptionalParam('decimals', "Define a decimals for balance formatting in the CSV table.", 6, types.int)
     .setAction(async ({ result, pps, csv, decimals }, hre) => {
-      console.log(`Using ${csv} table, PPS: ${pps}.`);
+      console.log(`Using ${csv} table, PPS: ${pps}`);
       const csvUsers = await parseHoldersAndBalances(
         "receiver",
         "balance",
