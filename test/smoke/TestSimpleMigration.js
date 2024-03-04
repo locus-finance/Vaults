96dc0bfbf6a54af36cae8799c4f99e03e036718e
Oleg Bedrin <numeralhuman@gmail.com>
2024-03-04 18:48:48 +0400

    adjusting simple migration test

--- test/smoke/TestSimpleMigration.js
+++ test/smoke/TestSimpleMigration.js
@@ -24,53 +24,59 @@ const withImpersonatedSigner = async (signerAddress, action) => {
 }
 
 describe('TestSimpleMigration', () => {
-    const userAddress = "0x263a8a1297582c307A008DE90372fbC98bEC1BA8";
-    const userXDefiAllowance = hre.ethers.BigNumber.from("1888597014");
-    const userXEthAllowance = hre.ethers.BigNumber.from("1349475625648352000");
+  const userAddress = "0x263a8a1297582c307A008DE90372fbC98bEC1BA8";
+  const userXDefiAllowance = hre.ethers.utils.parseUnits("352", 6); // 1222000000 // 1888597014
+  const userXEthAllowance = hre.ethers.BigNumber.from("1349475625648351995"); // 1349475625648352000
 
-    let simpleMigrationForXEthInstance;
-    let simpleMigrationForXDefiInstance;
-    let migrationsOwner;
-    let xEthInstance;
-    let xDefiInstance;
+  let simpleMigrationForXEthInstance;
+  let simpleMigrationForXDefiInstance;
+  let migrationsOwner;
+  let xEthInstance;
+  let xDefiInstance;
 
-    beforeEach(async () => {
-        simpleMigrationForXEthInstance = await hre.ethers.getContractAt(
-            "SimpleMigration",
-            "0x9C073294BaFcD23150bA3364DaBE37571b47Dabd"
-        );
-        simpleMigrationForXDefiInstance = await hre.ethers.getContractAt(
-            "SimpleMigration",
-            "0x381D91367317A9edb49C770ffaDe27FE91c977F5"
-        );
-        migrationsOwner = await simpleMigrationForXDefiInstance.owner();
-        xEthInstance = await hre.ethers.getContractAt(
-            "IERC20",
-            await simpleMigrationForXEthInstance.vaultV2()
-        );
-        xDefiInstance = await hre.ethers.getContractAt(
-            "IERC20",
-            await simpleMigrationForXDefiInstance.vaultV2()
-        );
-        await mintNativeTokens(migrationsOwner, "0x10000000000000000000");
+  beforeEach(async () => {
+    simpleMigrationForXEthInstance = await hre.ethers.getContractAt(
+      "SimpleMigration",
+      "0x9C073294BaFcD23150bA3364DaBE37571b47Dabd"
+    );
+    simpleMigrationForXDefiInstance = await hre.ethers.getContractAt(
+      "SimpleMigration",
+      "0x381D91367317A9edb49C770ffaDe27FE91c977F5"
+    );
+    migrationsOwner = await simpleMigrationForXDefiInstance.owner();
+    xEthInstance = await hre.ethers.getContractAt(
+      "IERC20",
+      await simpleMigrationForXEthInstance.vaultV2()
+    );
+    xDefiInstance = await hre.ethers.getContractAt(
+      "IERC20",
+      await simpleMigrationForXDefiInstance.vaultV2()
+    );
+    await mintNativeTokens(migrationsOwner, "0x10000000000000000000");
+  });
+
+  const testCaseForUserForXEth = (user, amount) => it('should migrate xEth for the user', async () => {
+    await withImpersonatedSigner(migrationsOwner, async (owner) => {
+      console.log(`xEth before: ${(await xEthInstance.balanceOf(user)).toString()}`);
+      const migrateUserTx = await simpleMigrationForXEthInstance.connect(owner).migrateUser(user, amount);
+      await migrateUserTx.wait();
+      console.log(`xEth after: ${(await xEthInstance.balanceOf(user)).toString()}`);
     });
+  });
 
-    it('should migrate xEth for the user', async () => {
-        await withImpersonatedSigner(migrationsOwner, async (owner) => {
-            console.log(`xEth before: ${(await xEthInstance.balanceOf(userAddress)).toString()}`);
-            const migrateUserTx = await simpleMigrationForXEthInstance.connect(owner).migrateUser(userAddress, userXEthAllowance);
-            await migrateUserTx.wait();
-            console.log(`xEth after: ${(await xEthInstance.balanceOf(userAddress)).toString()}`);
-        });
+  const testCaseForUserForXDefi = (user, amount) => it('should migrate xDefi for the user', async () => {
+    console.log(amount.toString());
+    stop = (await xDefiInstance.balanceOf(user)).gt(0);
+    await withImpersonatedSigner(migrationsOwner, async (owner) => {
+      console.log(`xDefi before: ${(await xDefiInstance.balanceOf(user)).toString()}`);
+      const migrateUserTx = await simpleMigrationForXDefiInstance.connect(owner).migrateUser(user, amount);
+      await migrateUserTx.wait();
+      console.log(`xDefi after: ${(await xDefiInstance.balanceOf(user)).toString()}`);
     });
+    return await xDefiInstance.balanceOf(user);
+  });
 
-    it('should migrate xDefi for the user', async () => {
-        await withImpersonatedSigner(migrationsOwner, async (owner) => {
-            console.log(`xDefi before: ${(await xDefiInstance.balanceOf(userAddress)).toString()}`);
-            const migrateUserTx = await simpleMigrationForXDefiInstance.connect(owner).migrateUser(userAddress, userXDefiAllowance);
-            await migrateUserTx.wait();
-            console.log(`xDefi after: ${(await xDefiInstance.balanceOf(userAddress)).toString()}`);
-        });
-    });
+  testCaseForUserForXEth(userAddress, userXEthAllowance);
+  testCaseForUserForXDefi(userAddress, userXDefiAllowance);
 });
 
